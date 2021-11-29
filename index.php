cee34fac96eab37b77bdfbedd637c954be8f2c89
Florian <dyckeuss@outlook.fr>
2021-11-29 17:11:59 +0100

    ajout index.php + style.css

---
+++ index.php
@@ -0,0 +1,71 @@
+<html>
+
+<head>
+    <title>Bon Mange Lakay</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand">Au bon Resto</a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav">
+                <a href="#" class="nav-item nav-link active">Réserver une Table</a>
+                <a href="#" class="nav-item nav-link">Entrée</a>
+                <a href="#" class="nav-item nav-link">Plats</a>
+                <a href="#" class="nav-item nav-link" tabindex="-1">Desserts</a>
+            </div>
+            <div class="navbar-nav ms-auto">
+                <a href="#" class="nav-item nav-link">Connexion</a>
+                <a href="#" class="nav-item nav-link">Inscription</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container footer">
+
+    <footer class="text-lg-start mt-xl-5 pt-4 text-white">
+
+        <p class="footer">© 2021 Au Bon Resto / Bon Manjé Lakay</p>
+
+    </footer>
+
+</div>
+
+
+
+</body>
+
+
+
+</html>
