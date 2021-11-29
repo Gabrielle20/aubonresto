659f3ecc5eddb2d747b46f36e48b2c95680af955
Gabrielle20 <gabriellecaeiro@gmail.com>
2021-11-29 15:43:33 +0100

    add autoloader

---
+++ Core/Autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+class Autoload 
+{
+
+    // récupère les classes
+    public static function autoloader($class) {
+        $class = str_replace("\\", "/", $class);
+        require ROOT."/$class.php";
+    }
+
+
+    // automatisation des requires
+    public static function register() {
+        spl_autoload_register(array(__CLASS__, 'autoloader'));
+    }
+}
