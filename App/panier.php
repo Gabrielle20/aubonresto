9360c66dd25688d9a989a4d398161c0b53a8f431
nekkache <hakimnekkache@gmail.com>
2021-11-29 15:59:37 +0100

    ajout des methodes

--- App/panier.php
+++ App/panier.php
@@ -8,4 +8,53 @@ class Panier
     private string $statut;
     private string $contenu;
     private string $user;
+
+
+
+    public function getTotal()
+    {
+        return $total;
+    }
+
+    public function setTotal()
+    {
+        $this->total = $total;
+    }
+
+
+
+    public function getStatut()
+    {
+        return $statut;
+    }
+
+    public function setStatut()
+    {
+        $this->statut = $statut;
+    }
+
+
+
+
+    public function getContenu()
+    {
+        return $contenu;
+    }
+
+    public function setContenu()
+    {
+        $this->contenu = $contenu;
+    }
+
+
+    public function getUser()
+    {
+        return $user;
+    }
+
+    public function setUser()
+    {
+        $this->user = $user;
+    }
+
 }
