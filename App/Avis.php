41d901cb32cb00d610f79df1322a76009fa16a59
nekkache <hakimnekkache@gmail.com>
2021-12-03 11:36:12 +0100

    creation page Avis.php

--- App/Avis.php
+++ App/Avis.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <div class="rating rating2">
-        <a href="?stars=5" title="Donner 5/5">★</a>
-        <a href="?stars=4" title="Donner 4/5">★</a>
-        <a href="?stars=3" title="Donner 3/5">★</a>
-        <a href="?stars=2" title="Donner 2/5">★</a>
-        <a href="?stars=1" title="Donner 1/5">★</a>
+    <div>
+        <form action="" method="post">
+            <label for="commentaire">Laisser un commentaire</label>
+            <textarea name="commentaire" id="commentaire" cols="40" rows="4"></textarea>
+            <a id="stars" href="?stars=1" title="Donner 1/5">★</a>
+            <a id="stars" href="?stars=2" title="Donner 2/5">★</a>
+            <a id="stars" href="?stars=3" title="Donner 3/5">★</a>
+            <a id="stars" href="?stars=4" title="Donner 4/5">★</a>
+            <a id="stars" href="?stars=5" title="Donner 5/5">★</a>
+            <button>Envoye</button>
+        </form>
     </div>
 </body>
+
 </html>
--- Templates/Panier/paiement.php
+++ Templates/Panier/paiement.php
@@ -4,7 +4,7 @@
 <?php include './Templates/headHtml.html'; ?>
 
 <body>
-<?php include('./Templates/nav.php');?>
+<?php include './Templates/nav.php';?>
     <div class="containerPaiement" class="mx-auto" style="width: 1100px;">
         <div class="rowPaiement">
             <form method="post" action="../getpanier.php">
@@ -35,12 +35,12 @@
     </div>
 
 
-    <?php include '../Templates/footerHtml.html' ?>
+
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
-    <?php include "./Templates/footerHtml.html"?>
+    <?php include './Templates/footerHtml.html'; ?>
 </body>
 
 </html>
